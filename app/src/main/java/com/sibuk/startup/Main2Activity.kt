5d4822a1c11b14f06bccacd2364b0947cf1aa990
sibu kumar behera <sibukumar98@gmail.com>
2019-08-26 18:46:29 +0530

    2nd activity updated

--- app/src/main/java/com/sibuk/startup/Main2Activity.kt
+++ app/src/main/java/com/sibuk/startup/Main2Activity.kt
@@ -2,6 +2,7 @@ package com.sibuk.startup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main2.*
 
@@ -23,37 +24,77 @@ class Main2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val final_list :ArrayList<Int> = intent.getIntArrayExtra("Result").toCollection(ArrayList())
+        val final_list: ArrayList<Int> = intent.getIntArrayExtra("Result").toCollection(ArrayList())
 //        val final_list :ArrayList<Int> = ArrayList()
 
         rm = final_list[0]
         rc = final_list[1]
         ra = final_list[2]
 
-        bm = final_list[3]
-        bc = final_list[4]
-        ba = final_list[5]
+        gm = final_list[3]
+        gc = final_list[4]
+        ga = final_list[5]
 
-        gm = final_list[6]
-        gc = final_list[7]
-        ga = final_list[8]
+        bm = final_list[6]
+        bc = final_list[7]
+        ba = final_list[8]
 
         textView2.text = "Slope : " + rm.toString()
-        textView3.text ="Intercept : " + rc.toString()
+        textView3.text = "Intercept : " + rc.toString()
         textView.text = "Accuracy : " + ra.toString()
 
-        textView5.text = "Slope : " + bm.toString()
-        textView6.text = "Intercept : " +  bc.toString()
-        textView11.text = "Accuracy : " + ba.toString()
+        textView5.text = "Slope : " + gm.toString()
+        textView6.text = "Intercept : " + gc.toString()
+        textView11.text = "Accuracy : " + ga.toString()
 
-        textView8.text = "Slope : " + gm.toString()
-        textView9.text = "Intercept : " +  gc.toString()
-        textView10.text = "Accuracy : " + ga.toString()
+        textView8.text = "Slope : " + bm.toString()
+        textView9.text = "Intercept : " + bc.toString()
+        textView10.text = "Accuracy : " + ba.toString()
 
-        button1.setOnClickListener(){
-
-
-
+        buttonYR.setOnClickListener() {
+            val n : Int = editText1.text.toString().toInt()
+            val n2 : Int = n * rm + rc
+            editText2.text = Editable.Factory.getInstance().newEditable("Y : $n2")
+        }
+        buttonXR.setOnClickListener() {
+            val n : Int = editText2.text.toString().toInt()
+            if(rm == 0){
+                editText1.text = Editable.Factory.getInstance().newEditable("X : NULL")
+            }
+            else {
+                val n2: Int = (n - rc) / rm
+                editText1.text = Editable.Factory.getInstance().newEditable("X : $n2")
+            }
+        }
+        buttonYG.setOnClickListener() {
+            val n : Int = editText3.text.toString().toInt()
+            val n2 : Int = n * gm + gc
+            editText4.text = Editable.Factory.getInstance().newEditable("Y : $n2")
+        }
+        buttonXG.setOnClickListener() {
+            val n : Int = editText4.text.toString().toInt()
+            if(gm == 0){
+                editText3.text = Editable.Factory.getInstance().newEditable("X : NULL")
+            }
+            else {
+                val n2: Int = (n - gc) / gm
+                editText3.text = Editable.Factory.getInstance().newEditable("X : $n2")
+            }
+        }
+        buttonYB.setOnClickListener() {
+            val n : Int = editText5.text.toString().toInt()
+            val n2 : Int = n * bm + bc
+            editText6.text = Editable.Factory.getInstance().newEditable("Y : $n2")
+        }
+        buttonXB.setOnClickListener() {
+            val n : Int = editText6.text.toString().toInt()
+            if(bm == 0){
+                editText5.text = Editable.Factory.getInstance().newEditable("X : NULL")
+            }
+            else {
+                val n2: Int = (n - bc) / bm
+                editText5.text = Editable.Factory.getInstance().newEditable("X : $n2")
+            }
         }
     }
 }
